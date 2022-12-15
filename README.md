f456492b0d931966014173c9d583a32a5e3d5c58
Rodrigo Kunz <raokrodrigo@gmail.com>
2022-12-15 17:35:09 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# 🚀 Trilha Explorer
+
+![preview](./github/preview.png)
+
+> Design of an application that calculates your BMI, based on your weight and height!
+
+[🔗Click to acess](https://rodkunz.github.io/BMI-calculator-IMC/)
+
+## 🛠 Technologies used
+
+- JavaScript
+- HTML
+- CSS
+- Git e Github
+
+## 💻 Contact
+
+raokrodrigo@gmail.com
---
+++ js/script.js
@@ -0,0 +1,37 @@
+import { Modal } from './modal.js'
+import { alertError } from './alert-error.js'
+import { calculateIMC, notNumber } from './utils.js'
+
+// Variables
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+inputWeight.oninput = () => alertError.close()
+inputHeight.oninput = () => alertError.close()
+
+form.onsubmit = event => {
+  event.preventDefault()
+
+  const weight = inputWeight.value
+  const height = inputHeight.value
+
+  const weightOrHeightIsNotANumber = notNumber(weight) || notNumber(height)
+  if (weightOrHeightIsNotANumber) {
+    alertError.open()
+    return;
+  }
+
+  alertError.close()
+
+  const result = calculateIMC(weight, height)
+  displayResultMessage(result)
+
+}
+
+function displayResultMessage(result) {
+  const message = `Seu IMC é de ${result}`
+
+  Modal.message.innerText = message
+  Modal.open()
+}
